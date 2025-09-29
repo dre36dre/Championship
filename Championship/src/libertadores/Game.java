b25c88d3f6474a2f13444acb83f0586f4b8e629b
Anderson Freires <dre.son36@gmail.com>
2025-09-29 10:22:04 -0300

    game class

--- Championship/src/libertadores/Game.java
+++ Championship/src/libertadores/Game.java
@@ -1,3 +1,5 @@
+package libertadores;
+
 
 public class Game {
 	protected String home;
@@ -5,6 +7,94 @@ public class Game {
 	protected boolean won=false ;
 	protected float time;
 	protected boolean underway;
+	protected int scoreboard;
+	
+	
+	public Game(String home, String visitor, boolean won, float time, boolean underway, int scoreboard) {
+		super();
+		this.home = home;
+		this.visitor = visitor;
+		this.won = won;
+		this.time = time;
+		this.underway = underway;
+		this.scoreboard = scoreboard;
+	}
+
+
+	public String getHome() {
+		return home;
+	}
+
+
+	public void setHome(String home) {
+		this.home = home;
+	}
+
+
+	public String getVisitor() {
+		return visitor;
+	}
+
+
+	public void setVisitor(String visitor) {
+		this.visitor = visitor;
+	}
+
+
+	public boolean isWon() {
+		return won;
+	}
+
+
+	public void setWon(boolean won) {
+		this.won = won;
+	}
+
+
+	public float getTime() {
+		return time;
+	}
+
+
+	public void setTime(float time) {
+		this.time = time;
+	}
+
+
+	public boolean isUnderway() {
+		return underway;
+	}
+
+
+	public void setUnderway(boolean underway) {
+		this.underway = underway;
+	}
+
+
+	public int getScoreboard() {
+		return scoreboard;
+	}
+
+
+	public void setScoreboard(int scoreboard) {
+		this.scoreboard = scoreboard;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Game [home=" + home + ", visitor=" + visitor + ", won=" + won + ", time=" + time + ", underway="
+				+ underway + ", scoreboard=" + scoreboard + "]";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
+
 
---
+++ Championship/src/libertadores/main.java
@@ -0,0 +1,8 @@
+package libertadores;
+
+public class main {
+	public static void main(String[] args) {
+		Game g[]= new Game[40];
+		g[0]=new Game(null, null, false, 0, false, 0)
+	}
+}
